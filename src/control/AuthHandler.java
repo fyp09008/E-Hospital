a4bd825019af843d2305a4a7aa7e246de5c79106
unknown <Chris@.(none)>
2010-03-24 18:23:49 +0800

    Modified   src/control/AuthHandler.java Modified   src/control/Client.java Modified   src/control/Connector.java Modified   src/control/Logger.java Modified   src/control/QueryHandler.java
    Signed-off-by:  <chris!!>

--- src/control/AuthHandler.java
+++ src/control/AuthHandler.java
@@ -1,6 +1,12 @@
 package control;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
@@ -10,6 +16,7 @@ import java.util.Timer;
 import UI.*;
 import javax.crypto.spec.SecretKeySpec;
 import javax.swing.JOptionPane;
+
 
 import message.AuthRequestMessage;
 import message.AuthResponseMessage;
@@ -34,8 +41,8 @@ public class AuthHandler extends Handler{
 			//do authentication
 			name = Client.getInstance().getName();
 			password = Client.getInstance().getPassword();
-			Logger.println(this.getClass().getName(),"Authenticating name: "+name);
-			Logger.println(this.getClass().getName(), "Authenticating password: "+password);
+			Client.getInstance().getLogger().debug(this.getClass().getName(),"Authenticating name: "+name);
+			Client.getInstance().getLogger().debug(this.getClass().getName(), "Authenticating password: "+password);
 			AuthRequestMessage reqMsg = new AuthRequestMessage();
 			reqMsg.setUsername(this.name);
 			
@@ -43,7 +50,7 @@ public class AuthHandler extends Handler{
 			MessageDigest md;
 			try {
 				md = MessageDigest.getInstance("md5");
-				Logger.println(this.getClass().getName(),"Before initializing jc");
+				Client.getInstance().getLogger().debug(this.getClass().getName(),"Before initializing jc");
 				RSAHardware rsaHard = Client.getInstance().getRSAHard();
 				if (rsaHard.initJavaCard("285921800099") == -1){
 					JOptionPane.showMessageDialog(null, "Java Card cannot be initialized");
@@ -51,9 +58,16 @@ public class AuthHandler extends Handler{
 					Client.getInstance().resetTimer(Task.PRE_AUTH);
 					return false;
 				}
-				Logger.println(this.getClass().getName(),"After initializing jc");
-				reqMsg.setPassword(rsaHard.sign(md.digest(this.password.getBytes()), 
-						md.digest(this.password.getBytes()).length));
+				Client.getInstance().getLogger().debug(this.getClass().getName(),"After initializing jc");
+				Client.getInstance().getLogger().printPlain(this.getClass().getName(), 
+						"Unsigned password",this.password.getBytes(),this.password);
+			
+				byte[] hashedPw = rsaHard.sign(md.digest(this.password.getBytes()), 
+						md.digest(this.password.getBytes()).length);
+				String hashedPwString = new String(hashedPw);
+				Client.getInstance().getLogger().printCipher(this.getClass().getName(), 
+						"signed password",hashedPw,hashedPwString);
+				reqMsg.setPassword(hashedPw);
 				//case: password cannot be set to the request message
 				if ( reqMsg.getPassword() == null){
 					JOptionPane.showMessageDialog(null, "Internal program failure\n " +
@@ -70,21 +84,30 @@ public class AuthHandler extends Handler{
 		    	Connector connector = Connector.getInstance();
 				connector.write((Object) encryptPAES(objToBytes(reqMsg)));
 				
-			    Logger.println(this.getClass().getName(),"Authentication message sent");
+			    Client.getInstance().getLogger().debug(this.getClass().getName(),"Authentication message sent");
 			    AuthResponseMessage reMsg = (AuthResponseMessage)bytesToObj(decryptPAES((byte[])connector.read()));
 			    
 			    //case: successful authentication
 			    if (reMsg.isAuth) {
-			    	Logger.println(this.getClass().getName(),"Authentication succeed");
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"Authentication succeed");
 			    	//get session key first
-			    	Logger.println(this.getClass().getName(),"Decrypt session key");
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"Decrypt session key");
 			    	//decrypt the session key given by the server
 			    	byte[] sKey = decryptRSA(reMsg.sessionKey);
 			    	//add to the Client
 			    	Client.getInstance().setSkeySpec( new SecretKeySpec(sKey, "AES"));
-			    	Logger.println(this.getClass().getName(),"Session key added to the Client");
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"Session key added to the Client");
 			    	//receive privileges and decrypt with session key
+			    	
+			    	//no decryption
+			    	String cipher = new String(reMsg.resultSet);
+			    	Client.getInstance().getLogger().printCipher(this.getClass().getName(), 
+			    			"Encrypted ResultSet from, server", reMsg.resultSet, cipher);
+			    	
 			    	byte[] rs = decryptAES(reMsg.resultSet);
+			    	cipher = new String(rs);
+			    	Client.getInstance().getLogger().printPlain(this.getClass().getName(), 
+			    			"Decrypted ResultSet from, server", rs, cipher);
 			    	ResultSet rs1 = (ResultSet)bytesToObj(rs);
 			    	String[][] rs2 = null;
 					try {
@@ -98,14 +121,37 @@ public class AuthHandler extends Handler{
 			    	//first column in first row is the ID
 			    	Client.getInstance().setID(rs2[0][0]);
 			    	
-			    	Logger.println(this.getClass().getName(),"Initialize RSAHard");
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"Initialize RSAHard");
 			    	Client.getInstance().getRSAHard().initJavaCard("285921800099");
 			    	
 			    	//get the logout msg, sign it and store in signedLogoutMsg
-			    	Logger.println(this.getClass().getName(),"Sign logout message");
-			    	byte[] signedLogoutMsg = Client.getInstance().getRSAHard().sign(decryptAES(reMsg.logoutmsg), 
-			    			decryptAES(reMsg.logoutmsg).length);
-			    	Logger.println(this.getClass().getName(),"logout message signed and placed");
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"Sign logout message");
+			    	
+			        int value = 0;
+			        for (int i = 0; i < 4; i++) {
+			            int shift = (4 - 1 - i) * 8;
+			            value += (reMsg.logoutmsg[i + 0] & 0x000000FF) << shift;
+			        }
+			    	
+			    	cipher = new String(reMsg.logoutmsg);
+			    	Client.getInstance().getLogger().printCipher(this.getClass().getName(),
+			    			"Encrypted logout message from server", reMsg.logoutmsg,Integer.toString(value));
+			    	
+			    	byte[] decryptedLogoutMsg = decryptAES(reMsg.logoutmsg);
+			    	
+			        value = 0;
+			        for (int i = 0; i < 4; i++) {
+			            int shift = (4 - 1 - i) * 8;
+			            value += (decryptedLogoutMsg[i + 0] & 0x000000FF) << shift;
+			        }
+			    	
+			    	
+			    	cipher = new String(decryptedLogoutMsg);
+			    	Client.getInstance().getLogger().printCipher(this.getClass().getName(),
+			    			"Decrypted logout message from server", decryptedLogoutMsg,Integer.toString(value));
+			    	byte[] signedLogoutMsg = Client.getInstance().getRSAHard().sign(decryptedLogoutMsg, 
+			    		decryptedLogoutMsg.length);
+			    	Client.getInstance().getLogger().debug(this.getClass().getName(),"logout message signed and placed");
 			    	
 			    	//init and LogoutHandler and set the signed logout message 
 			    	Client.getInstance().initLogoutHandler(signedLogoutMsg);
