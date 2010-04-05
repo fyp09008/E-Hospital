d1fa37286a87fb31fb44d99a5a6359610c75b5f7
unknown <Gilbert@.(none)>
2010-04-05 18:39:20 +0800

    Program Auth completed but untested Deleted    notes/1.txt Modified   src/Driver.java Add        common.jar
    Signed-off-by:  <Gilbert>

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,55 @@
+import java.io.FileNotFoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Date;
 import java.util.Timer;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import remote.obj.ProgramAuthHandler;
+
+import cipher.ProKeyGen;
 
 import UI.*;
 import control.*;
 
-public class Driver{
+public class Driver {
+	
+	public static int selfAuth(String serverPath) {
+		try {
+			ProKeyGen pkg = new ProKeyGen("common.jar");
+			Registry reg = LocateRegistry.getRegistry(serverPath);
+			ProgramAuthHandler pah = (ProgramAuthHandler)reg.lookup(serverPath);
+			return pah.authProgram(pkg.getNo(), pkg.getProgramKey().getEncoded());
+		} catch (FileNotFoundException e) {
+			JOptionPane.showMessageDialog(new JFrame(), "The common.jar is not found. Please don't rename the program.");
+			e.printStackTrace();
+			return 4;
+		} catch (RemoteException e) {
+			JOptionPane.showMessageDialog(new JFrame(), "The server is unreachable. Please check your network connection.");
+			e.printStackTrace();
+			return 8;
+		} catch (NotBoundException e) {
+			JOptionPane.showMessageDialog(new JFrame(), "The server is unreachable. Please check your network connection.");
+			e.printStackTrace();
+			return 16;
+		}
+		
+	}
+	
 	public static void main(String[] argv){
+		String serverPath = null;
+		if(argv.length != 0) 
+			serverPath = argv[0];
+		else 
+			serverPath = "localhost";
+		if (selfAuth(serverPath) != 0) {
+			System.exit(1);
+		}
+		
 		//SoftCard sd = new SoftCard();
 		Timer t = new Timer();
 		Date now = new Date();
